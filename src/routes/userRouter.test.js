3680d4cd2d3cdb9b8629379696c91a3a5976db8f
egriffioen <ellagriffioen@gmail.com>
2026-01-30 15:12:59 -0700

    fix(lint error)

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -1,7 +1,5 @@
 const request = require('supertest');
 const app = require('../service');
-const { DB } = require('../database/database.js');
-
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
