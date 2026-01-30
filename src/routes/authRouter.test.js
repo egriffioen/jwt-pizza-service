3cf6589e0a474c54d63846efe004c268d30d069d
egriffioen <ellagriffioen@gmail.com>
2026-01-29 19:15:18 -0700

    fix(linter errors)

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -52,10 +52,6 @@ function expectValidJwt(potentialJwt) {
   expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 }
 
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
-}
-
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
   jest.setTimeout(60 * 1000 * 5); // 5 minutes
 }
