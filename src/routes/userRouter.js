77832d7ad6ee225d22bb149124758a4887da8c28
egriffioen <ellagriffioen@gmail.com>
2026-02-17 23:49:00 -0700

    add(delete api) requires admin

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -81,6 +81,9 @@ userRouter.delete(
   '/:userId',
   authRouter.authenticateToken,
   asyncHandler(async (req, res) => {
+    if (!req.user.isRole(Role.Admin)) {
+      return res.status(403).json({ message: 'unauthorized' });
+    }
     const userId = Number(req.params.userId);
     await DB.deleteUser(userId);
     res.json({ message: 'user deleted' });
